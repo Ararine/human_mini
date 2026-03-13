52c559085d5631ca42c8cfddc4974b2863481bcd
minyoung <miny007708@gmail.com>
2026-03-13 17:42:06 +0900

    minyoung 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Map from "./pages/Map";
 
 function App() {
-  return (
-    <>
-      <header>
-        <div class="logo">logo</div>
-        <div class="search">search</div>
-        <div class="user_tab">
-          <div class="login">login</div>
-        </div>
-      </header>
-      <main>main</main>
-    </>
-  );
+  return <Signup />;
 }
 
 export default App;
