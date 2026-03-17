741eb1bb2368c987ac6b954d7c6493fb7434204b
cjcho <qkdrk7777775@gmail.com>
2026-03-17 11:15:14 +0900

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Map />} />
+      {/* aaaa */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/find-id" element={<FindId />} />
