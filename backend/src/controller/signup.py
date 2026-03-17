7025bc0dfccd50659135e101afeab3c71dde8051
정진화 <pisterjin@naver.com>
2026-03-17 17:48:54 +0900

    아이디 중복 체크

--- backend/src/controller/signup.py
+++ backend/src/controller/signup.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request, status
 from fastapi.responses import JSONResponse
 from sqlalchemy import text
 
-from service.signup import register_user
+from service.signup import register_user, heck_duplicate_username
 from util.database import engine  # DB 연결 
 from schema.signup import SignupRequest
 
@@ -25,3 +25,27 @@ async def register(data: SignupRequest):
     except Exception as e:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, 
                             content={"error": str(e)})
+        
+
+# 회원 가입 시 아이디 중복 확인 API (controller의 signup.py)
+@router.get("/check-username/{username}")
+async def check_username(username:str): 
+    try:
+        # 클라이언트에서 받은 username을 디비와 비교하여 동일한 값이 있으면 리턴
+        is_duplicate = check_duplicate_username(username)
+        if is_duplicate:
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content = {"isDuplicate":True, "message":"이미 사용 중인 아이디입니다."}
+            )
+        return JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content = {"isDuplicate":False, "messages":"사용 가능한 아이디입니다."}
+        ) 
+    except Exception as e:
+        return JSONResponse(
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content = {"error":str(e)}
+        )
+               
+        
--- backend/src/route/signup.py
+++ backend/src/route/signup.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
-from controller.signup import register
-
+from controller.signup import register, check_username
 
 router = APIRouter()
 
-router.post("/register")(register)         
+router.post("/register")(register)          
+router.get("/check-username/{username}")(check_username)
