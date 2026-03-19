bbf062ccc606ed8b6647ca160ab11630c878767a
lmhyuck <doccabiss@gmail.com>
2026-03-19 17:38:41 +0900

    회원가입시 인증 테이블 정보 자동 삭제

--- backend/src/service/signup.py
+++ backend/src/service/signup.py
@@ -3,7 +3,7 @@ import bcrypt
 
 from model.user_handler import create_user, get_user
 from util.database import SessionLocal
-
+from model.verification_handler import delete_verification_status
 
 # 회원 등록  
 async def register_user(
@@ -36,8 +36,14 @@ async def register_user(
             phone_number = phone_number,
             telecom_provider = telecom_provider,
         )
-        if not result:                        
-            raise Exception("회원가입 실패") 
+        # 2. 회원가입이 성공(True)했을 때만 실행
+        if result:
+            # [핵심] 인증 테이블에서 해당 이메일의 모든 임시 기록 삭제
+            delete_verification_status(db, email=email)
+            print(f"회원가입 완료: {email} 관련 인증 데이터 삭제 성공")
+        else:
+            # result가 False이거나 None일 경우 실패 처리
+            raise Exception("회원가입 처리 중 오류가 발생했습니다.")
     finally:
         db.close()
 
